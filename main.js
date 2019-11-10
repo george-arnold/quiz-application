6adebf8890f114417c83c05ace269ad501225c69
George Arnold <george.arnold.88@gmail.com>
2019-11-09 21:32:45 -0500

    changing opening screen to first question is functional

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const STORE = [
     {
-        question: 'What percentage of high school students in the US are enrolled in vocational programs?' 
+        question: 'What percentage of high school students in the US are enrolled in vocational programs?',
         answers: [
             '32%','6%','15%','65%'
         ],
@@ -12,12 +12,18 @@ const STORE = [
 let score = 0;
 let currentQuestion = 0;
 
-function startHandler {
-//handles the START button
 
+    
+function startHandler() {
+//handles the START button
+$('.js-start-button').click(function() {
+ generateQuestion(STORE[currentQuestion].question,STORE[currentQuestion].answers);
+    });
 }
-function generateQuestion(questionItem) {
-//generates HTML that will print when the button is clicked
+
+function generateQuestion(questionItem,answerChoices) {
+$(".js-question-box").html('<form> <fieldset> <legend class="question">' + questionItem + '</legend> </fieldset> </form>');
+console.log(answerChoices);
 }
 
 function updateQuestionNumber() {
@@ -31,12 +37,13 @@ function submitAnswerHandler() {
 //check user answer against real answer
 //if correct: prompt that user is correct, add 1 to the score
 //if incorrect: prompt the correct answer
-
+}
 function nextQuestion() {
 //advance to next question-->generateQuestion
 currentQuestion++;
 generateQuestion(STORE[currentQuestion.question]);
 //update the question number and the score number
+//if final question, call showFinalScreen
 }
 
 function showFinalScreen(){
@@ -48,4 +55,7 @@ function restartQuiz(){
 generateQuestion(0);
 }
 
-
+function startQuizApp() {
+    startHandler();
+}
+$(startQuizApp)
